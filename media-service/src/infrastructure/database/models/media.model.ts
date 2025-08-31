8631bd592f9d120fd36ce3bc1445833330b7ebd9
Mohammed Bilal N S <70318383+mohammedbilalns@users.noreply.github.com>
2025-08-31 18:43:32 +0530

    chore(media-service) : updated media schema to store category and the usage details

--- media-service/src/infrastructure/database/models/media.model.ts
+++ media-service/src/infrastructure/database/models/media.model.ts
@@ -5,9 +5,14 @@ export interface IMedia extends Document, Omit<Media, "id"> {}
 
 export const mediaSchema = new Schema({
 	mediaType:{type:String,enum:["image","audio","document"], default:"image"},
+	category:{type:String,enum:["profile", "article","chat", "resume"]},
 	publicId:{type:String,required:true},
 	originalName:{type:String,required:true},
 	url:{type:String,required:true},
+	size:{type:Number,required:true},
+	articleId:{type:String},
+	chatMessageId:{type:String},
+	mentorId:{type:String},
 	userId:{type:String,required:true}
 },{ timestamps: true });
 
