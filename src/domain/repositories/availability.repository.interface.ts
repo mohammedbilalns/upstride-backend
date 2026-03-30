0eacb366930b62cf8b3129c9ef0ed977552418af
mohammedbilalns <70318383+mohammedbilalns@users.noreply.github.com>
2026-03-30 15:54:49 +0530

    chore : updated domain entities , repo interfaces

--- src/domain/repositories/availability.repository.interface.ts
+++ src/domain/repositories/availability.repository.interface.ts
@@ -14,10 +14,15 @@ export interface IAvailabilityRepository
 	extends CreatableRepository<Availability>,
 		FindByIdRepository<Availability>,
 		UpdatableByIdRepository<Availability> {
-	findByMentorId(mentorId: string): Promise<Availability[]>;
+	findByMentorId(
+		mentorId: string,
+		options?: { activeOnly?: boolean; expired?: boolean },
+	): Promise<Availability[]>;
+
 	findActiveByMentorIdAndDate(
 		mentorId: string,
 		date: Date,
 	): Promise<Availability[]>;
+	updateStatus(id: string, status: boolean): Promise<void>;
 	deleteById(id: string): Promise<void>;
 }
