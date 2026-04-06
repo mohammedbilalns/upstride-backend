8264675419dc03bbaa078ae5b6a29175b176e536
mohammedbilalns <70318383+mohammedbilalns@users.noreply.github.com>
2026-04-06 11:01:45 +0530

    chore : proper types for article reaction mapper

--- src/infrastructure/database/mongodb/mappers/article-reaction.mapper.ts
+++ src/infrastructure/database/mongodb/mappers/article-reaction.mapper.ts
@@ -2,17 +2,32 @@ import { Types } from "mongoose";
 import { ArticleReaction } from "../../../../domain/entities/article-reaction.entity";
 import type { ArticleReactionDocument } from "../models/article-reaction.model";
 
+type PopulatedUserRef = {
+	_id: Types.ObjectId;
+	name?: string;
+};
+
+export type ArticleReactionDocumentWithUser = Omit<
+	ArticleReactionDocument,
+	"userId"
+> & {
+	userId: Types.ObjectId | PopulatedUserRef;
+};
+
+const isPopulatedUserRef = (
+	userId: Types.ObjectId | PopulatedUserRef,
+): userId is PopulatedUserRef => {
+	return typeof userId === "object" && userId !== null && "_id" in userId;
+};
+
 export class ArticleReactionMapper {
-	static toDomain(doc: any): ArticleReaction {
+	static toDomain(doc: ArticleReactionDocumentWithUser): ArticleReaction {
 		const userId = doc.userId;
-		const actorName =
-			userId && typeof userId === "object" && "name" in userId
-				? (userId as any).name
-				: undefined;
-		const userIdStr =
-			userId && typeof userId === "object" && "_id" in userId
-				? (userId as any)._id.toString()
-				: userId.toString();
+		const isPopulatedUser = isPopulatedUserRef(userId);
+		const actorName = isPopulatedUser ? userId.name : undefined;
+		const userIdStr = isPopulatedUser
+			? userId._id.toString()
+			: userId.toString();
 
 		return new ArticleReaction(
 			doc._id.toString(),
